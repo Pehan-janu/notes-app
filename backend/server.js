8d70a6d6a56f0da82213d248dcedfe94608d7be3
Pehan Januka <pehanjanu@gmail.com>
2025-08-28 12:45:55 +0530

    Add .gitignore and remove node_modules from tracking

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
+console.log('server.js is being executed');
 import express from 'express';
+//if we want to use import then we have to add "type": "module" in package.json file
+//const express = require("express"); //this is common js module system
+const app = express();
+
+app.listen(5001, () => {
+  console.log("Server started on PORT: 5001");
+});
