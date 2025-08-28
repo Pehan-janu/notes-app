585203fdc67c98d260cb853d345b7bd077a518d6
Pehan Januka <pehanjanu@gmail.com>
2025-08-28 12:57:20 +0530

    Delete server.js

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-console.log('server.js is being executed');
-import express from 'express';
-//if we want to use import then we have to add "type": "module" in package.json file
-//const express = require("express"); //this is common js module system
-const app = express();
-
-app.listen(5001, () => {
-  console.log("Server started on PORT: 5001");
-});
