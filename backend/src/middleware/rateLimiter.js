61af8bb1cfc4aa82c910d6a67cd4c5b797a2fbb0
Pehan Januka <pehanjanu@gmail.com>
2025-09-18 23:48:00 +0530

    create a RateLimit

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,22 @@
+import ratelimit from "../config/upstash.js";
+
+
+const rateLimiter = async (req,res,next) => {
+  //this is per user rate limiting
+
+  try{
+    const {success} = await ratelimit.limit("my-limit-key");
+
+    if(!success){
+      return res.status(429).json({message: "Too many requests, please try again later"});
+    }
+
+    next();
+
+  } catch (error){
+    console.log("Rate limit error", error);
+    next(error);
+  };
+};
+
+export default rateLimiter;
