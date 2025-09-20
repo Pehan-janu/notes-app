4f4d91e7ab9503fc56709e4b96b20aa121405327
Pehan Januka <pehanjanu@gmail.com>
2025-09-20 18:54:58 +0530

    Update a home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import NavBar from "../components/NavBar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import { useEffect } from "react";
+import axios from "axios";
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("https://localhost:5173/api/notes");
+        console.log(res.data);
+      } catch (error) {
+        console.log("Error fetching notes:");
+      }
+    };
+
+    fetchNotes();
+  }, []);
   return (
     <div className="min-h-screen">
-      <Navbar />
+      <NavBar />
+
+      {isRateLimited && <RateLimitedUI />}
     </div>
   );
 };
 
-export default HomePage
+export default HomePage;
